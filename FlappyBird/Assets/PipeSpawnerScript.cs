366cf201a0996695bd0f16ba987b72fa70d31912
José Wytalo <112438631+JwapWytalo@users.noreply.github.com>
2023-11-10 17:00:31 -0300

    Add files via upload

---
+++ FlappyBird/Assets/PipeSpawnerScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnerScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    public float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(pipe, transform.position, transform.rotation);
+            timer = 0;
+        }
+
+        Instantiate(pipe, transform.position, transform.rotation);
+        
+    }
+}
